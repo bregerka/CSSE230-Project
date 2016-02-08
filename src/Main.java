2ceedd1aa4215209303fdf1f2231186895001f45
alexapieragowski <pieragab@rose-hulman.edu>
2016-02-08 10:35:52 -0500

    Test code in main
    Doesn't do anything

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,16 @@
+import java.awt.BorderLayout;
+import java.awt.Container;
+import java.awt.Font;
 
-public class Main {
+import javax.swing.JFrame;
 
+public class Main extends JFrame {
+	
+
+	public Main() {
+		Container window = getContentPane();
+	    window.setLayout(new BorderLayout());
+	    Font myfont = new Font("arial", Font.BOLD, 36);
+	    
+	}
 }
