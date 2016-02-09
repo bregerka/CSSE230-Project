8dac6d342542203dcab1f83976e8bb603eaa77a8
alexapieragowski <pieragab@rose-hulman.edu>
2016-02-09 10:39:13 -0500

    Minutes, window
    Minutes from 2/9/16, got a test window up in main

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,31 @@
 import java.awt.BorderLayout;
-import java.awt.Container;
-import java.awt.Font;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 public class Main extends JFrame {
 	
 
 	public Main() {
-		Container window = getContentPane();
+		
+	}
+	
+	public static void main(String[] args){
+		JFrame window = new JFrame("Testing");
+		window.setDefaultCloseOperation(EXIT_ON_CLOSE);
 	    window.setLayout(new BorderLayout());
-	    Font myfont = new Font("arial", Font.BOLD, 36);
-	    
+	    window.setSize(1000, 1000);
+	    JLabel label = new JLabel("pls work", JLabel.CENTER);
+	    window.getContentPane().add(label);
+//	    Font myfont = new Font("arial", Font.BOLD, 36);
+	    window.setVisible(true);
+		
+//		EventQueue.invokeLater(new Runnable() {
+//			public void run(){
+//			Main test = new Main();
+//			test.setVisible(true);
+//		}
+//		});
+		
 	}
 }
