affe95a70b4dc2e2f5a6a700efe032294e282ce9
alexapieragowski <pieragab@rose-hulman.edu>
2016-02-11 09:39:20 -0500

    Still trying to fix it

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.awt.EventQueue;
 import java.awt.FlowLayout;
 
 import javax.swing.ImageIcon;
@@ -8,28 +9,28 @@ public class Main extends JFrame {
 	
 
 	public Main() {
-		
-	}
-	
-	public static void main(String[] args){
 		JFrame window = new JFrame("Testing");
 		window.setDefaultCloseOperation(EXIT_ON_CLOSE);
 	    window.setLayout(new FlowLayout());
 	    window.setSize(1000, 1000);
 	    JLabel label = new JLabel("pls work", JLabel.CENTER);
 	    window.getContentPane().add(label);
-	    JLabel imageLabel = new JLabel("?", JLabel.BOTTOM);
+	    JLabel imageLabel = new JLabel("?", JLabel.RIGHT);
 	    imageLabel.setIcon(new ImageIcon("skyrim test map.jpg"));
 	    window.getContentPane().add(imageLabel);
 //	    Font myfont = new Font("arial", Font.BOLD, 36);
-	    window.setVisible(true);
+//	    window.setVisible(true);
+	}
+	
+	public static void main(String[] args){
 		
-//		EventQueue.invokeLater(new Runnable() {
-//			public void run(){
-//			Main test = new Main();
-//			test.setVisible(true);
-//		}
-//		});
+		
+		EventQueue.invokeLater(new Runnable() {
+			public void run(){
+			Main test = new Main();
+			test.setVisible(true);
+		}
+		});
 		
 	}
 }
