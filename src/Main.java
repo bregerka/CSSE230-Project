09001de01dd0733a0b6d9816abe86adf9390fa17
alexapieragowski <pieragab@rose-hulman.edu>
2016-02-11 09:32:58 -0500

    More graphics

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,4 @@
-import java.awt.BorderLayout;
-java.awt.*;
-import java.awt.Dimension;
-import java.awt.Image;
-import java.awt.Label;
+import java.awt.FlowLayout;
 
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
@@ -18,20 +14,14 @@ public class Main extends JFrame {
 	public static void main(String[] args){
 		JFrame window = new JFrame("Testing");
 		window.setDefaultCloseOperation(EXIT_ON_CLOSE);
-	    window.setLayout(new BorderLayout());
-//	    window.setSize(1000, 1000);
+	    window.setLayout(new FlowLayout());
+	    window.setSize(1000, 1000);
 	    JLabel label = new JLabel("pls work", JLabel.CENTER);
-//	    window.getContentPane().add(label);
-//	    Label imageLabel = new Label();
-//	    imageLabel.setIcon(new ImageIcon("skyrim test map.jpg"));
-//	    window.getContentPane().add(imageLabel);
-	    Image image;
-	    public void paintComponent(Graphics g){
-	        super.paintComponent(g);
-	        if(image != null){
-	            g.drawImage(image, 0, 0, this);
-	        }
-	    }
+	    window.getContentPane().add(label);
+	    JLabel imageLabel = new JLabel("?", JLabel.BOTTOM);
+	    imageLabel.setIcon(new ImageIcon("skyrim test map.jpg"));
+	    window.getContentPane().add(imageLabel);
+//	    Font myfont = new Font("arial", Font.BOLD, 36);
 	    window.setVisible(true);
 		
 //		EventQueue.invokeLater(new Runnable() {
