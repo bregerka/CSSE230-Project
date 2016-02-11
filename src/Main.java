79a6db4be09f0aba0ac5655e0badf839b7143a8c
alexapieragowski <pieragab@rose-hulman.edu>
2016-02-11 09:29:02 -0500

    Added more graphic stuff to main, added a sample map image

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 import java.awt.BorderLayout;
+java.awt.*;
+import java.awt.Dimension;
+import java.awt.Image;
+import java.awt.Label;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
@@ -14,10 +19,19 @@ public class Main extends JFrame {
 		JFrame window = new JFrame("Testing");
 		window.setDefaultCloseOperation(EXIT_ON_CLOSE);
 	    window.setLayout(new BorderLayout());
-	    window.setSize(1000, 1000);
+//	    window.setSize(1000, 1000);
 	    JLabel label = new JLabel("pls work", JLabel.CENTER);
-	    window.getContentPane().add(label);
-//	    Font myfont = new Font("arial", Font.BOLD, 36);
+//	    window.getContentPane().add(label);
+//	    Label imageLabel = new Label();
+//	    imageLabel.setIcon(new ImageIcon("skyrim test map.jpg"));
+//	    window.getContentPane().add(imageLabel);
+	    Image image;
+	    public void paintComponent(Graphics g){
+	        super.paintComponent(g);
+	        if(image != null){
+	            g.drawImage(image, 0, 0, this);
+	        }
+	    }
 	    window.setVisible(true);
 		
 //		EventQueue.invokeLater(new Runnable() {
